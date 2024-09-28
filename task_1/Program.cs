a6117b10283e223eafdef8fa744d2135b5023b02
Tamerlan62 <tamerlan.recebov062@gmail.com>
2024-09-28 22:36:10 +0400

    save all tasks

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+if (M > N)
+{
+    Console.WriteLine("Ошибка: M должно быть меньше или равно N.");
+    return;
+}
+
+PrintNumbers(M, N);
+
+void PrintNumbers(int current, int N)
+{
+    if (current > N)
+        return;
+
+    Console.WriteLine(current);
+    PrintNumbers(current + 1, N);
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите неотрицательное число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите неотрицательное число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+try
+{
+    long result = Ackermann(m, n);
+    Console.WriteLine($"A({m}, {n}) = {result}");
+}
+catch (StackOverflowException)
+{
+    Console.WriteLine("Ошибка: слишком большие значения m и n.");
+}
+
+long Ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m == 1)
+        return n + 2;
+    else if (m == 2)
+        return 2 * n + 3;
+    else if (m == 3)
+        return (1 << (n + 3)) - 3; // 2^(n+3) - 3 для ускорения
+    else
+        return Ackermann(m - 1, (int)Ackermann(m, n - 1)); // Приводим к int
+}
